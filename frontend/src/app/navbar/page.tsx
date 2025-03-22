e9ec8c8637af63dbcb9cb9cbaac461cdb8b447aa
quagrain <victorquagraine9@gmail.com>
2025-03-22 17:16:56 +0000

    Shadcn import & navbar

---
+++ frontend/src/app/navbar/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+
+export default function NavBar() {
+  return (
+    <nav className="flex justify-between p-4 bg-black">
+      <Link className="ml-2 navbar-brand text-primary tracking-tight text-3xl font-bold" href="/">
+        Food Finds
+      </Link>
+
+      <div className="btn-group space-x-3">
+        <Button variant="secondary">Sign Up</Button>
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
+}
